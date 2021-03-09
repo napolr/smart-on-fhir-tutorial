e20e150e7c8fb5a1dee2bee4f680337793893fb0
napolr <42223213+napolr@users.noreply.github.com>
2021-03-09 15:45:55 -0600

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -49,7 +49,7 @@
           p.fname = fname;
           p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
-
+	  p.allergies=getAllergyIntolerances(patient);
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
           }
@@ -115,10 +115,10 @@
     }
   }
   
-  function getAllergyIntolerances(){
+  function getAllergyIntolerances(patient){
     
     
-        var allergyIntolerance = smart.patient.api.fetchAll({
+        var allergyIntolerance = patient.api.fetchAll({
                       type: 'AllergyIntolerance',                    
                     });
         allergies=null;
