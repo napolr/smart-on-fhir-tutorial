20af254b80df961c9e42de08640a16e173c25f55
napolr <42223213+napolr@users.noreply.github.com>
2021-03-09 14:26:03 -0600

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -43,7 +43,7 @@
           var ldl = byCodes('2089-1');
 
           var p = defaultPatient();
-          p.patientId=patient.id;
+          p.patientId=patient.patientId;
           p.birthdate = patient.birthDate;
           p.gender = gender;
           p.fname = fname;
@@ -116,7 +116,52 @@
     }
   }
   
- 
+  function getAllergyIntolerances(){
+    
+    
+        var allergyIntolerance = smart.patient.api.fetchAll({
+                      type: 'AllergyIntolerance',                    
+                    });
+        allergies=null;
+    	if ( allergyIntolerance !== null ){
+			var allergyTableHeader="<table><tr><td>item</td><td>category</td><td>reaction</td></tr>";
+			var j=0;
+			allergyRows="";
+			var rows="";
+			allergyIntolerance.forEach(function(allergy,j){
+				 
+				
+			//	log.debug(JSON.stringify(allergy));
+					//log.debug("allergy.resource.code"+JSON.stringify(allergy.resource));
+					
+					if (allergy.resource.code && allergy.resource.code!="invalid"){
+						rows+="<tr><td>"+allergy.resource.code.text+"</td><td>"+allergy.resource.category+"</td><td>";
+					}
+					 
+					var i=0;
+					if ( allergy.resource.reaction){
+						allergyReactions=""; 
+						allergy.resource.reaction.forEach(function(reaction){
+							
+						   if  (i===0){ 
+							  allergyReactions=reaction.description+ "("+reaction.severity+")";
+						   } else {
+							 allergyReactions=", " + reaction.description + "("+reaction.severity+")";
+						   } 
+						});
+						rows+="<td>"+allergyReactions+"</td>"; 
+						rows+="</tr>";
+					} 
+				  // log.debug("rows="+rows);
+				  
+					
+		  });
+		  //log.debug("allergies="+rows);
+		  allergies=allergyTableHeader+ rows+ "</table>";
+         return(allergies);
+    }
+}
+
   window.drawVisualization = function(p) {
     $('#holder').show();
     $('#loading').hide();
