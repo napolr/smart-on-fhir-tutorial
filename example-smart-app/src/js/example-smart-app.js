e5eab3540154b6602b546e51685a03882dd6339d
napolr <russmarketinggroup@gmail.com>
2021-03-11 15:16:15 -0600

    test

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -198,7 +198,7 @@
     async function callRestfulAPI(relativeURL, token, contentType, acceptType) {
         let data = await getResult(relativeURL, token, contentType, acceptType);
         console.log(data)
-        return JSON.parse(data);
+        return JSON.stringify(data);
     }
     
 /*
