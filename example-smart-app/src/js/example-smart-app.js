3f9271087011eacc7929c58e515e8ea58adbab22
napolr <russmarketinggroup@gmail.com>
2021-03-13 13:47:29 -0600

    test

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -45,7 +45,7 @@
                 fhirResults = [];
                 fhirAPIs.forEach(function (apiCall) {
                     var relativeURL = smart.server.serviceUrl + "/" + apiCall + "?_id=" + patient.id;
-                   let  result= await doAPICall(relativeURL, token, "application/json", "application/json")                   
+                   let  result= doAPICall(relativeURL, token, "application/json", "application/json")                   
                         .then(response => {
                             console.log(response);
                             return response;
